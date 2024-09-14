38f2ae63f2809972fcb8d89cd2fe8975208ce876
cjy007 <17741653+cjy007@users.noreply.github.com>
2024-09-14 16:21:01 +0800

    push

--- app/dash/page.tsx
+++ app/dash/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
-  return <h1>dash page111!</h1>
+function Page() {
+  return <h1>dash page!</h1>
 }
+function Page1() {
+  return <h1>dash page111!
+    <Page/>
+      </h1>
+}
+
+export default Page1
