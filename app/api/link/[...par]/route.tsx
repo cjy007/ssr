3783baacb9257489df273c8db77721e7f5575a2b
root <root@GT-PC-173>
2024-10-10 21:03:06 +0800

    1

---
+++ app/api/link/[...par]/route.tsx
@@ -0,0 +1,28 @@
+import type { NextRequest } from 'next/server'
+
+export const runtime = 'edge'
+
+
+export async function GET(request: NextRequest, params: string) {
+  let responseText = `{
+    "flag": "success",
+    "status": "success",
+    "message": {
+        "error_code": "200",
+        "error_msg": "操作成功",
+        "debug_msg": "",
+        "msg_zh": "操作成功"
+    },
+    "result": {
+        "modelName": "111",
+        "version": "AIFS3.0_V1.0.1_240419.1"
+    }
+  }`
+
+  console.log("params:",params)
+
+  var resp = new Response(responseText)
+
+  resp.headers.set("content-type", "application/json")
+  return resp
+}
